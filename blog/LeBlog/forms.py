fad751bfdb5195154f7a7677b610b59e74d9570e
Myriuss <marielelyamani@gmail.com>
2023-02-26 00:46:04 +0100

    :white_check_mark: bootstrap added for forms

---
+++ blog/LeBlog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Titre'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- blog/LeBlog/views.py
+++ blog/LeBlog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 # listView allows to list a query set to the datebase
@@ -20,6 +21,7 @@ class PostDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
     #fields = '__all__'
-    fields = ('title', 'body')
+    #fields = ('title', 'body')
