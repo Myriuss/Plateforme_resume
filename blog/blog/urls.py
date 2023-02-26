7bd152a262a5f4cc8dd2041180da3330c928d4c2
Myriuss <marielelyamani@gmail.com>
2023-02-26 22:04:05 +0100

    :white_check_mark: authentification added

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -6,5 +6,7 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("LeBlog.urls")),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include("members.urls")),
 ]
 
