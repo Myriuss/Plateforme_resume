173aa61d741073d90c9a051dd0bfebfd1494f897
Myriuss <marielelyamani@gmail.com>
2023-03-03 21:35:49 +0100

    :sparkles: unlike button

--- blog/LeBlog/views.py
+++ blog/LeBlog/views.py
@@ -14,7 +14,14 @@ from django.http import HttpResponseRedirect
 
 def LikeView (request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists() :
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('post-details', args=[str(pk)]))
 
 class HomeView(ListView):
@@ -45,8 +52,14 @@ class PostDetailView(DetailView):
         context = super(PostDetailView, self).get_context_data(*args, **kwargs)
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
         context["cat_menu"] = cat_menu
         context["total_likes"] = total_likes
+        context["liked"] = liked
         return context
 
 
