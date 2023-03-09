74b891a07a6970bf6db411ff456c7671583ac20a
Myriuss <marielelyamani@gmail.com>
2023-03-09 19:20:40 +0100

    :sparkles: ckeditor

--- blog/LeBlog/models.py
+++ blog/LeBlog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 class Categorie(models.Model):
         name = models.CharField(max_length=255)
@@ -17,7 +18,8 @@ class Post(models.Model):
         title = models.CharField(max_length=255)
         title_tag = models.CharField(max_length=255)
         author = models.ForeignKey(User, on_delete=models.CASCADE)
-        body = models.TextField()
+        body = RichTextField(blank=True, null=True)
+        #body = models.TextField()
         post_date = models.DateField(auto_now_add=True)
         categorie = models.CharField(max_length=255)
         likes = models.ManyToManyField(User, related_name='resumee')
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'LeBlog',
     'members',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
