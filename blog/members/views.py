f29732708c2c1d001a280c309aeb405f95be5878
Myriuss <marielelyamani@gmail.com>
2023-03-10 23:50:57 +0100

    :white_check_mark: edit profile

--- blog/members/views.py
+++ blog/members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
 
@@ -8,3 +8,11 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('hone')
+
+    def get_object(self):
+        return self.request.user
