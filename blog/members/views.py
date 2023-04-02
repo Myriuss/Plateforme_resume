6a9a69e06177fae441122456a84a0f2db31fa744
Myriuss <marielelyamani@gmail.com>
2023-04-02 14:48:10 +0000

    :sparkles: Style Our Profile Page With Bootstrap

--- blog/members/views.py
+++ blog/members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
@@ -10,7 +10,7 @@ class UserRegisterView(generic.CreateView):
     success_url = reverse_lazy('login')
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 
