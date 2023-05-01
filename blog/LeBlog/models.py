e343193a908a554cc5d2c0803f2f48dae46851a5
Myriuss <marielelyamani@gmail.com>
2023-05-02 00:24:42 +0100

    :sparkles: Add Profile Pic and Social Media Links

--- blog/LeBlog/models.py
+++ blog/LeBlog/models.py
@@ -17,6 +17,11 @@ class Categorie(models.Model):
 class Profile(models.Model):
         user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
         bio = models.TextField()
+        profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+
+        microsoft_teams_url = models.CharField(max_length=255, null=True, blank=True)
+        github_url = models.CharField(max_length=255, null=True, blank=True)
+        linkedin_url = models.CharField(max_length=255, null=True, blank=True)
 
         def __str__(self):
             return str(self.user)
