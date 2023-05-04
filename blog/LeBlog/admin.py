f5ee0d43215ba8d9de46fb417ec848c7a849df3f
Myriuss <marielelyamani@gmail.com>
2023-05-04 21:06:10 +0100

    :white_check_mark: Comment section added

--- blog/LeBlog/admin.py
+++ blog/LeBlog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Categorie, Profile
+from .models import Post, Categorie, Profile, Comment
 
 admin.site.register(Post)
 admin.site.register(Categorie)
 admin.site.register(Profile)
+admin.site.register(Comment)
--- blog/LeBlog/models.py
+++ blog/LeBlog/models.py
@@ -56,3 +56,12 @@ class Post(models.Model):
 def save(self, *args, **kwargs):
         self.name = self.name.lower()
         return super(Categorie, self).save(*args, **kwargs)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name= "comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
